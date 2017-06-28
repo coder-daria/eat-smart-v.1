3f46b46d1c4aaf78c35eb7e6a4b572867d44b12f
Darii <me.darilla@gmail.com>
2017-06-28 15:46:49 +0200

    Organising files and cleaning code by using react-charts.js

--- src/Chart.js
+++ src/Chart.js
@@ -1,41 +1,29 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
+import {countGrams, countKcal} from './functions';
 
-var chartData = {
-    data: {
-        labels: ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [
-            {
-                label: 'My First Dataset',
-                fill: true,
-                lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderCapStype: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: 'black',
-                pointStyle: 'rectRounded',
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 5,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,191,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: [65, 59, 80, 81, 56, 55, 40]
-            }
-        ]
-    }
+var goal = countKcal(2000, 20, 30, 50);
+var diet = countGrams(goal);
+
+var data = {
+    labels: ["Fat", "Proteins", "Carbs"],
+    datasets: [
+        {
+            label: "Calories",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 2,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: [4, diet.proteins, diet.carbs],
+        }
+    ],
 };
-
 
 class Chart extends React.Component {
     render() {
         return (
-            <Line data={chartData.data} width="600" height="250" />
+            <Line data={data} width={600} height={250} />
         )
     }
 }
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,21 @@
-export function sum(x, y){
+function sum(x, y){
   return x + y;
+}
+
+export function countKcal(calories, fat, proteins, carbs) {
+  var howMuchKcal = {
+    fat: calories * (fat / 100),
+    proteins: calories * (proteins / 100),
+    carbs: calories * (carbs / 100)
+  }
+  return howMuchKcal;
+}
+
+export function countGrams(aliment) {
+  var howMuchCalories = {
+    fat: Math.floor(aliment.fat / 9),
+    proteins: Math.floor(aliment.proteins / 4),
+    carbs: Math.floor(aliment.carbs / 4)
+  }
+  return howMuchCalories;
 }
