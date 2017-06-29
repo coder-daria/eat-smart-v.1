a3e1e8bbd975016d7eaafea2ef77272dbacefbac
Daria <me.darilla@gmail.com>
2017-06-29 14:33:41 +0200

    creating aliment and portion functions, writing tests

--- src/functions.js
+++ src/functions.js
@@ -1,17 +1,35 @@
-export function countKcal(calories, fat, proteins, carbs) {
-  var howMuchKcal = {
-    fat: calories * (fat / 100),
-    proteins: calories * (proteins / 100),
-    carbs: calories * (carbs / 100)
-  }
-  return howMuchKcal;
+export function caloriesPerDay(calories, fat, proteins, carbs) {
+    var howMuchKcal = {
+        fat: calories * (fat / 100),
+        proteins: calories * (proteins / 100),
+        carbs: calories * (carbs / 100)
+    }
+    return howMuchKcal;
 }
 
 export function countGrams(aliment) {
-  var howMuchCalories = {
-    fat: Math.floor(aliment.fat / 9),
-    proteins: Math.floor(aliment.proteins / 4),
-    carbs: Math.floor(aliment.carbs / 4)
-  }
-  return howMuchCalories;
+    var howMuchCalories = {
+        fat: Math.floor(aliment.fat / 9),
+        proteins: Math.floor(aliment.proteins / 4),
+        carbs: Math.floor(aliment.carbs / 4)
+    }
+    return howMuchCalories;
+}
+
+function aliment(fat, proteins, carbs) { // Nutritions in grams
+    const aliment = {
+        fat: fat * 9,
+        proteins: proteins * 4,
+        carbs: carbs * 4
+    }
+    return aliment; // Object contains nutritions in kcal
+}
+
+function portion(aliment, quantity) { //Aliment contains nutritions in grams
+    const portion = {
+        fat: aliment.fat * quantity / 100,
+        proteins: aliment.proteins * quantity / 100,
+        carbs: aliment.carbs * quantity / 100
+    }
+    return portion;
 }
--- src/functions.test.js
+++ src/functions.test.js
@@ -1,21 +1,41 @@
-import {countKcal, countGrams} from './functions';
+import { caloriesPerDay, countGrams } from './functions';
 
 it('counts calories', () => {
-  expect(countKcal(2000, 20,30, 50)).toEqual({
-    fat: 400,
-    proteins: 600,
-    carbs: 1000
-  });
+    expect(caloriesPerDay(2000, 20, 30, 50)).toEqual({
+        fat: 400,
+        proteins: 600,
+        carbs: 1000
+    });
 });
 
 it('counts grams', () => {
-  expect(countGrams({
-    fat: 400,
-    proteins: 600,
-    carbs: 1200
-  })).toEqual({
-    fat: 44,
-    proteins: 150,
-    carbs: 300
-  })
+    expect(countGrams({
+        fat: 400,
+        proteins: 600,
+        carbs: 1200
+    })).toEqual({
+        fat: 44,
+        proteins: 150,
+        carbs: 300
+    })
+})
+
+it('creates an aliment object', () => {
+    expect(aliment(2, 6, 51)).toEqual({
+        fat: 18,
+        proteins: 24,
+        carbs: 204
+    })
+})
+
+it('creates a portion object', () => {
+    expect(portion({
+        fat: 2,
+        proteins: 6,
+        carbs: 51
+    }, 300)).toEqual({
+        fat: 6,
+        proteins: 18,
+        carbs: 153
+    })
 })
