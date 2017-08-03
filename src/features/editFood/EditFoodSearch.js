72b11fbb45dd348dfc5455244472334ba9f1a95e
Daria <me.darilla@gmail.com>
2017-08-03 20:30:43 +0200

    bugfix: edit food form is updated after submit

--- src/features/editFood/EditFoodSearch.js
+++ src/features/editFood/EditFoodSearch.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import AutoComplete from '../../common/AutoComplete';
 import PropTypes from 'prop-types';
-
 class EditFoodSearch extends React.Component {
+  state = {
+    idOfFoodSelected: null
+  }
+  componentWillReceiveProps(newProps) {
+    if (this.props.foods !== newProps.foods) {
+      newProps.onSelect(newProps.foods.filter(f => f.properties.id === this.state.idOfFoodSelected)[0]);
+    }
+  }
   render() {
     // let picture = (item) => {
     //   if (item.properties && item.properties.url) {
     //     return <img alt={item.name} src={item.properties.url} width="200" height="100" />
     //   }
-    // }
+    // }                   
     return (
       <div>
         Search
         <AutoComplete
           items={this.props.foods}
           onSelect={(val, food) => {
+            this.setState({ idOfFoodSelected: food.properties.id });
             this.props.onSelect(food)
           }}
         />
@@ -24,8 +32,8 @@ class EditFoodSearch extends React.Component {
 }
 
 EditFoodSearch.propTypes = {
-    foods: PropTypes.array,
-    onSelect: PropTypes.func,
+  foods: PropTypes.array,
+  onSelect: PropTypes.func,
 }
 
 export default EditFoodSearch;
