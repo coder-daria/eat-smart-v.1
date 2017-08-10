29d124968c1455b0f0d2d0dfe4c251d08b00e70c
Daria <me.darilla@gmail.com>
2017-08-10 14:55:12 +0200

    Auto complete in preferences

--- src/features/preferences/PreferenceForm.js
+++ src/features/preferences/PreferenceForm.js
@@ -1,31 +1,31 @@
 import React from 'react';
 // import TimePicker from 'rc-time-picker';
+// import 'rc-time-picker/assets/index.css';
 import TimePicker from 'material-ui/TimePicker';
-import 'rc-time-picker/assets/index.css';
+import AutoComplete from 'material-ui/AutoComplete';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
 class PreferenceForm extends React.Component {
   state = {
     name: "",
-    seconds: 0
+    seconds: 0,
+    dataSource: [] //`dataSource` is marked as required in `AutoComplete`;
   }
-  mealName = (event) => {
+  mealName = (value) => {
     this.setState({
-      name: event.target.value
+      name: value,
     })
   }
 
   handleTime = (time, object) => {
     let data = moment(object);
-    let chosenUnixTimestamp = (moment(data).unix()) * 1000; 
+    let chosenUnixTimestamp = (moment(data).unix()) * 1000;
 
     if (object === null) {
       return;
     }
-
     // let chosenUnixTimestamp = (moment(time).unix()) * 1000; // seconds
-
     this.setState({
       seconds: chosenUnixTimestamp
     })
@@ -41,10 +41,11 @@ class PreferenceForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSave}>
-        Meal name :<br /><input onChange={this.mealName} type="text" /><br />
-        <br />
-          {/* <TimePicker defaultValue={moment()} showSecond={false} onChange={this.handleTime} /><br />   */}
-          <TimePicker format="24hr" hintText="Choose time" onChange={this.handleTime}/>
+        {/* Meal name :<br /><input onChange={this.mealName} type="text" /> */}
+         Meal name :<br />  
+          <AutoComplete hintText="Type anything" dataSource={this.state.dataSource} onUpdateInput={this.mealName}/><br />
+         {/* <TimePicker defaultValue={moment()} showSecond={false} onChange={this.handleTime} /><br />    */}
+        <TimePicker format="24hr" hintText="Choose time" onChange={this.handleTime} />
         <button type="submit" id="saveButton">Save</button>
       </form>
     )
