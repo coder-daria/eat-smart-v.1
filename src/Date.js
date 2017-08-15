673f572b7bb20e9fc7b614322b2cdf3dea08682d
Daria <me.darilla@gmail.com>
2017-08-15 15:29:55 +0200

    adding IconButton to display calendar

--- src/Date.js
+++ src/Date.js
@@ -2,6 +2,9 @@ import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import RaisedButton from 'material-ui/RaisedButton';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import {cyan600} from 'material-ui/styles/colors';
 
 class Date extends React.Component {
     constructor(props) {
@@ -18,7 +21,7 @@ class Date extends React.Component {
         if (this.state.fullDate !== null) {
             let prevMonth = this.state.fullDate.getMonth();
             let currentMonth = date.getMonth();
-            
+
             if (prevMonth !== currentMonth) {
                 console.dir("New month!!");
             }
@@ -28,11 +31,13 @@ class Date extends React.Component {
         return "2017-08-12";
     }
     render() {
+
         let datePicker;
-        const button = <RaisedButton label="Calendar" type="submit" primary={true} onTouchTap={() => datePicker.focus()} />;
+        const calendarIcon = <IconButton type="submit" onTouchTap={() => datePicker.focus()} ><FontIcon className="material-icons" color={cyan600}>date_range</FontIcon></IconButton>;
+        // const button = <RaisedButton label={"Calendar"} type="submit" primary={true} onTouchTap={() => datePicker.focus()} />;
         return (
             <div>
-                {button}
+                {calendarIcon}
                 <DatePicker
                     hintText={this.today()}
                     value={this.state.controlledDate}
