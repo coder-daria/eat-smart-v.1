11d2249c6236bbb4fff942a6e1b66a56c14c97a8
Daria <me.darilla@gmail.com>
2017-08-23 12:09:19 +0200

    Cleaning App.js, adding TimeFor component

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import Time from './Time';
 import DayPickerContainer from './features/changeDate/DayPickerContainer';
 import MenuIcon from './MenuIcon';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import TimeFor from './TimeFor';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 class App extends Component {
   render() {
@@ -23,7 +23,7 @@ class App extends Component {
             <div className="appContainer">
               <div className="header">
                 <Time />
-                Time for dinner
+                <TimeFor />
                 <DayPickerContainer />
                 <MenuIcon />
               </div>
