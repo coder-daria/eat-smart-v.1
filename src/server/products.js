a6a735e9c038571c4666189aabc1f0bc50ee040e
Daria <me.darilla@gmail.com>
2017-08-29 16:15:13 +0200

    renaming food products

--- src/server/products.js
+++ src/server/products.js
@@ -1,116 +1,90 @@
-// PROTEIN
-export const jamonCuradoBodega = {
-  name: 'jamonCuradoBodega',
-  fat: 14,
-  protein: 31,
-  carbs: 1,
-  url: 'http://www.spain-recipes.com/images/jamon-iberico.jpg'
-};
-export const kefirMlekpol = {
-  name: 'kefirMlekpol',
+export const kefir = {
+  name: 'kefir',
   fat: 1.8,
   protein: 3.8,
   carbs: 5.2
 };
-export const kefirTesco = {
-  name: 'kefirTesco',
-  fat: 2,
-  protein: 3.2,
-  carbs: 4.7
+export const curd = {
+  name: 'curd',
+  fat: 4.3,
+  protein: 11,
+  carbs: 3.4
 };
-export const twarogChudyMlekpol = {
-  name: 'twarogChudyMlekpol',
-  fat: 0,
-  protein: 17,
-  carbs: 3.8
-};
-export const szprotWOlejuRzepakowym = {
-  name: 'szprotWOlejuRzepakowym',
+export const sprat = {
+  name: 'sprat in oil',
   fat: 19,
   protein: 20,
   carbs: 0
 };
-export const sardynkiTesco = {
-  name: 'sardynkiTesco',
+export const sardines = {
+  name: 'sardines',
   fat: 4,
   protein: 0,
   carbs: 0
 };
-export const cacaoMagnetic = {
-  name: 'cacaoMagnetic',
+export const cocoa = {
+  name: 'cocoa',
   fat: 10.5,
   protein: 23.5,
   carbs: 13
 };
-export const jajko = {
-  name: 'jajko',
+export const egg = {
+  name: 'egg',
   fat: 9.7,
   protein: 12.5,
   carbs: 0.6
 };
-export const serekWiejskiPilos = {
-  name: 'serekWiejskiPilos',
+export const cottage_cheese = {
+  name: 'cottage cheese',
   fat: 5,
   protein: 11,
   carbs: 2
 };
-export const gorzkaCzekoladaWawel = {
-  name: 'gorzkaCzekoladaWawel',
+export const dark_chocolate = {
+  name: 'dark chocolate',
   fat: 18,
   protein: 6,
   carbs: 14
 };
-export const otrębyPrzenne = {
-  name: 'otrębyPrzenne',
+export const wheat_bran = {
+  name: 'wheat bran',
   fat: 4.6,
   protein: 16,
   carbs: 20
 };
-export const platkiOwsianeSante = {
-  name: 'platkiOwsianeSante',
+export const oats = {
+  name: 'oats',
   fat: 7.2,
   protein: 12,
   carbs: 62
 };
-export const platkiZytniePelnoziarniste = {
-  name: 'platkiZytniePelnoziarniste',
+export const rye_flakes = {
+  name: 'rye flakes',
   fat: 1.6,
   protein: 8.2,
   carbs: 62
 };
-export const chlebZytniHarnas = {
-  name: 'chlebZytniHarnas',
-  fat: 1.5,
-  protein: 4.5,
-  carbs: 53
-};
-export const chlebZytniBezDrozdzy = {
-  name: 'chlebZytniBezDrozdzy',
+export const rye_bread = {
+  name: 'rye_bread',
   fat: 3.5,
   protein: 6.1,
   carbs: 42
 };
-export const chlebOrkiszowy = {
-  name: 'chlebOrkiszowy',
+export const spelled_bread = {
+  name: 'spelled bread',
   fat: 1.8,
   protein: 6.8,
   carbs: 43
 };
-export const kaszaGryczanaMelvit = {
-  name: 'kaszaGryczanaMelvit',
+export const buckwheat_groats = {
+  name: 'buckwheat groats',
   fat: 3,
   protein: 20,
   carbs: 61
 };
-export const ryzRisana = {
-  name: 'ryzRisana',
+export const rice = {
+  name: 'rice',
   fat: 3.2,
   protein: 12.1,
   carbs: 66.2
-};
-export const ryzJasminowyBritta = {
-  name: 'ryzJasminowyBritta',
-  fat: 0.7,
-  protein: 7.7,
-  carbs: 76
 };
