cad0ef23f27fc3d92b7d244b346ec23dcf3232f7
Daria <me.darilla@gmail.com>
2017-09-02 11:53:19 +0200

    creating VerticalDivider component

---
+++ src/features/addMeal/VerticalDivider.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class VerticalDivider extends React.Component {
+  render() {
+    return <div className="verticalDivider" />;
+  }
+}
+
+export default VerticalDivider;
